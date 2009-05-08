02f6d16ae74f138b6fd9cc1271203635a19b5a74
Jon Adams <jonadams@iluminada.com>
2009-05-08 00:20:33 -0700

    Cleaned up tabulation -- purely cosmetic

--- vimeo_source.php
+++ vimeo_source.php
@@ -1,24 +1,24 @@
 <?php
 /** 
- * Vimeo Datasource 0.1 
- * 
- * Vimeo datasource to communicate with the Vimeo Simple API (Advanced on the way...) 
- * 
- * 
- * Licensed under The MIT License 
- * Redistributions of files must retain the above copyright notice. 
- * 
- * 
- * @author Jon (pointlessjon) Adams <jon@anti-gen.com> 
- * @copyright (c) n/a
- * @license http://www.opensource.org/licenses/mit-license.php The MIT License 
- * @created May 7, 2009 
- * @version 0.1 * 
- */
- App::import('Core', array('HttpSocket'));
+* Vimeo Datasource 0.1 
+* 
+* Vimeo datasource to communicate with the Vimeo Simple API (Advanced on the way...) 
+* 
+* 
+* Licensed under The MIT License 
+* Redistributions of files must retain the above copyright notice. 
+* 
+* 
+* @author Jon (pointlessjon) Adams <jon@anti-gen.com> 
+* @copyright (c) n/a
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License 
+* @created May 7, 2009 
+* @version 0.1 * 
+*/
+App::import('Core', array('HttpSocket'));
  
- class VimeoSource extends DataSource {
- 
+class VimeoSource extends DataSource {
+
 	var $description = 'Vimeo Simple API';
 	var $Http = null;
 	var $allowedRequests = array(
@@ -58,22 +58,22 @@
 	);
 	
 	/** 
-     * Constructor sets configuration and instantiates HttpSocket
-     * 
-     * @param array config Optional. 
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Constructor sets configuration and instantiates HttpSocket
+	* 
+	* @param array config Optional. 
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function __construct($config = null) {
 		parent::__construct($config);
 		$this->Http =& new HttpSocket();
 	}
 	
 	/** 
-     * Retrieve data about a specific video
-     * 
-     * @param string videoId Required.
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve data about a specific video
+	* 
+	* @param string videoId Required.
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function video($videoId = null) {
 		if (!empty($videoId)) {
 			return $this->__vimeoApiRequest("clip/{$videoId}");
@@ -82,12 +82,12 @@
 	}
 	
 	/** 
-     * Retrieve data for a specific user
-     * 
-     * @param string username Required.
-	 * @param string request Required. See allowed requests in api documentation
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve data for a specific user
+	* 
+	* @param string username Required.
+	* @param string request Required. See allowed requests in api documentation
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function userRequest($username = null, $request = null) {
 		if (!empty($username) && !empty($request)) {
 			if (in_array($request, $this->allowedRequests['user'])) {
@@ -98,12 +98,12 @@
 	}
 	
 	/** 
-     * Retrieve activity data for a specific user
-     * 
-     * @param string username Required.
-	 * @param string request Required. See allowed requests in api documentation
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve activity data for a specific user
+	* 
+	* @param string username Required.
+	* @param string request Required. See allowed requests in api documentation
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function activityRequest($username = null, $request = null) {
 		if (!empty($username) && !empty($request)) {
 			if (in_array($request, $this->allowedRequests['activity'])) {
@@ -114,12 +114,12 @@
 	}
 	
 	/** 
-     * Retrieve data for a specific group
-     * 
-     * @param string groupname Required.
-	 * @param string request Required. See allowed requests in api documentation
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve data for a specific group
+	* 
+	* @param string groupname Required.
+	* @param string request Required. See allowed requests in api documentation
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function groupRequest($groupname = null, $request = null) {
 		if (!empty($groupname) && !empty($request)) {
 			if (in_array($request, $this->allowedRequests['group'])) {
@@ -130,12 +130,12 @@
 	}
 	
 	/** 
-     * Retrieve data for a specific channel
-     * 
-     * @param string channelname Required.
-	 * @param string request Required. See allowed requests in api documentation
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve data for a specific channel
+	* 
+	* @param string channelname Required.
+	* @param string request Required. See allowed requests in api documentation
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function groupRequest($channelname = null, $request = null) {
 		if (!empty($channelname) && !empty($request)) {
 			if (in_array($request, $this->allowedRequests['channel'])) {
@@ -146,12 +146,12 @@
 	}
 	
 	/** 
-     * Retrieve data for a specific album
-     * 
-     * @param string albumname Required.
-	 * @param string request Required. See allowed requests in api documentation
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Retrieve data for a specific album
+	* 
+	* @param string albumname Required.
+	* @param string request Required. See allowed requests in api documentation
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function groupRequest($albumname = null, $request = null) {
 		if (!empty($albumname) && !empty($request)) {
 			if (in_array($request, $this->allowedRequests['album'])) {
@@ -162,11 +162,11 @@
 	}
 	
 	/** 
-     * Internal function to make the requests to the Vimeo Simple API
-     * 
-     * @param string data Required.
-     * @see http://www.vimeo.com/api/docs/simple-api
-     */ 
+	* Internal function to make the requests to the Vimeo Simple API
+	* 
+	* @param string data Required.
+	* @see http://www.vimeo.com/api/docs/simple-api
+	*/ 
 	function __vimeoApiRequest($data = null) {
 		if (!empty($data)) {
 			return unserialize($this->Http->get("http://vimeo.com/api/{$data}.php", null));
@@ -174,5 +174,5 @@
 		return false;
 	}
  
- }
+}
 ?>
